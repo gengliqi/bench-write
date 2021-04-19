b82bd04ddaa32edfb4857c668358e98c82eba99e
gengliqi <gengliqiii@gmail.com>
2021-04-19 18:27:50 +0800

    change format
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    match args[0].as_str() {
-        "sync_write" => bench_sync_write::run_bench_sync_write(&args.as_slice()[1..]),
-        "async_fsync" => bench_async_fsync::run_bench_async_fsync(&args.as_slice()[1..]),
-        _ => panic!("{} argument invalid", args[0]),
+    match args[1].as_str() {
+        "sync_write" => bench_sync_write::run_bench_sync_write(&args.as_slice()[2..]),
+        "async_fsync" => bench_async_fsync::run_bench_async_fsync(&args.as_slice()[2..]),
+        _ => panic!("{} argument invalid", args[1]),
     }
 }
